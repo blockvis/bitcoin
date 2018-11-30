d9c64a5333d91d10acb02d1e9ced64a7e7d54af5
Oleg Kertanov <okertanov@gmail.com>
2018-11-30 15:02:08 +0200

    Mobile: iOS Integration test for BitcoinCore.framework

---
+++ mobile/ios/tests/integration/BitcoinCoreIntegration/ObjCBridge.m
@@ -0,0 +1,9 @@
+//
+//  ObjCBridge.m
+//  BitcoinCoreIntegration
+//
+//  Created by Oleg Kertanov on 30/11/2018.
+//  Copyright © 2018 Blockvis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ mobile/ios/tests/integration/BitcoinCoreIntegration/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BitcoinCoreIntegration
+//
+//  Created by Oleg Kertanov on 30/11/2018.
+//  Copyright © 2018 Blockvis. All rights reserved.
+//
+
+import UIKit
+import BitcoinCore
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
